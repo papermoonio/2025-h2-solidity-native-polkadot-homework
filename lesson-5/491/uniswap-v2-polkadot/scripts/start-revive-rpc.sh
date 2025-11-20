b6be696c9ae3a6df39b1a4922cf2db625110e575
Joe <vfptr@hotmail.com>
2025-11-20 23:05:59 +0800

    upload script for luanching nodes

--- lesson-5/491/uniswap-v2-polkadot/scripts/start-revive-rpc.sh
+++ lesson-5/491/uniswap-v2-polkadot/scripts/start-revive-rpc.sh
@@ -8,6 +8,7 @@ SCRIPT_DIR="$(cd "$(dirname "$SCRIPT_PATH")" && pwd)"
 echo "Script dir:  $SCRIPT_DIR"
 
 rm -rf "${SCRIPT_DIR}/logs/*"
+mkdir -p "${SCRIPT_DIR}/logs"
 
 ${SCRIPT_DIR}/../bin/revive-dev-node --dev --tmp --unsafe-rpc-external &> ${SCRIPT_DIR}/logs/revive-dev-node.log &
 ${SCRIPT_DIR}/../bin/eth-rpc --log eth-rpc=TRACE &> ${SCRIPT_DIR}/logs/rpc.log &
